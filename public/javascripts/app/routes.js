1c0d74e1871ea484326142c97cec23de37bc539d
William Lima Pereira <william.pereira@totvs.com.br>
2013-09-17 11:56:03 -0300

    AJUSTE DE CSS
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

--- public/javascripts/app/routes.js
+++ public/javascripts/app/routes.js
@@ -6,6 +6,7 @@ angular.module('myApp', ['myApp.controllers', 'myApp.resource']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home1', controller: 'HomeControle'});
     $routeProvider.when('/festa', {templateUrl: 'partials/home1', controller: 'FestaControle'});
+    $routeProvider.when('/restaurante', {templateUrl: 'partials/home1', controller: 'RestauranteControle'});    
     $routeProvider.when('/buffet', {templateUrl: 'partials/home1', controller: 'BuffetControle'});
     $routeProvider.when('/contato', {templateUrl: 'partials/home1', controller: 'ContatoControle'});
     $routeProvider.when('/localizacao', {templateUrl: 'partials/home1', controller: 'LocalControle'});
