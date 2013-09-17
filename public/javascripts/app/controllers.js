789c7ea8bb50ccfe4b5011622419112750e0fbc6
William Lima Pereira <william.pereira@totvs.com.br>
2013-09-17 11:02:42 -0300

    AJUSTE DE FRASES NO CONTEUDO DA HOME
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

--- public/javascripts/app/controllers.js
+++ public/javascripts/app/controllers.js
@@ -17,16 +17,17 @@ angular.module('myApp.controllers', []).
       var post2 = {};
       post2.img = "https://lh5.googleusercontent.com/-ZzJ0B1Drs_8/UidFKeXu-lI/AAAAAAAAAFY/eNeccJE7pIc/w274-h206-no/156363_480632925328620_1623402347_n.jpg";
       post2.title = "Buffet";
-      post2.description = "TRABALHAMOS NO NOSSO SALÃO PARA ATÉ 120 PESSOAS OU À DOMICILIO. CASAMENTOS, NOIVADOS, CONFRATERNIZAÇÕES, ANIVERSÁRIOS INFANTIS, JANTARES, ÓTIMOS PREÇOS FAÇA ORÇAMENTO SEM COMPROMISSO SANDRA.ARABE@IG.COM.BR TEL 4104-3888 NEXTEL 7914-4601 ID 927*24605";      $scope.posts.push(post2);
+      post2.description = "TRABALHAMOS NO NOSSO SALÃO PARA ATÉ 120 PESSOAS OU À DOMICILIO. CASAMENTOS, NOIVADOS, CONFRATERNIZAÇÕES, ANIVERSÁRIOS INFANTIS, JANTARES";
+      $scope.posts.push(post2);
 
       var post3 = {};
       post3.img = "https://lh3.googleusercontent.com/-v2vvS7TiRvI/UidFK_XdnkI/AAAAAAAAAFk/rlZQGMSqe8s/w640-h480-no/994555_562913853767193_1158598544_n.jpg";
       post3.title = "Delivery";
-      post3.description = "Comida Caseira. Entregamos ? Domicilio. Fa?a j? o seu pedido.";
+      post3.description = "Comida Caseira. Entregamos à Domicilio. Faça já o seu pedido.";
       $scope.posts.push(post3);
 
 
-     $scope.frase = "?TIMOS PRE?OS FA?A OR?AMENTO SEM COMPROMISSOSANDRA.ARABE@IG.COM.BR TEL 4104-3888 NEXTEL 7914-4601 ID 927*24605";
+     $scope.frase = "ÓTIMOS PREÇOS FAÇA ORÇAMENTO SEM COMPROMISSOSANDRA.ARABE@IG.COM.BR TEL 4104-3888 NEXTEL 7914-4601 ID 927*24605";
 
 
   }])
