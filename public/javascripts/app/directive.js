241427bc5c6796c82bacde0dd76f451a5260b942
William Lima Pereira <william.pereira@totvs.com.br>
2013-09-27 16:56:33 -0300

    INCLUS?O DE UMA DIRETIVA NO ANGULAR JS
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

--- public/javascripts/app/directive.js
+++ public/javascripts/app/directive.js
@@ -6,7 +6,13 @@ angular.module('myApp.directives', [])
 		restrict : 'A',
 		link : function(scope, element, attrs) {			
 			setTimeout(function() {
-					$(element).fotorama({width : 750, heigth : 750, autoplay:4000});					
+				
+				//Galleria.loadTheme('javascripts/vendors/galleria/theme/picasa/galleria.picasa.min.js');
+									
+				Galleria.run('.restaurantes-festas');
+				
+				
+					//$(element).fotorama({width : 750, heigth : 750, autoplay:4000});					
 			}, 100)						
 		}
 	}	
